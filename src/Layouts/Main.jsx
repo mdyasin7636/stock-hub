08c4cf5a24e5ae8ec7a09f22b8b81a2768206e67
mdyasin111 <mdyasin7636@gmail.com>
2023-09-10 11:36:58 +0600

    folder structure updated

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../Pages/Navbar/Navbar";
 
 const Main = () => {
     return (
         <div>
-            <Navbar/>
             <Outlet/>
         </div>
     );
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Login from "../../Login/Login";
 
 const Home = () => {
     return (
         <div>
-            <h1>This is Home</h1>
+            <Login/>
         </div>
     );
 };
