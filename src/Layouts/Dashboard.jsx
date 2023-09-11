779e2f2345c91d6907567f250350a96cd61a9735
tusarallen <allentusar909@gmail.com>
2023-09-11 10:56:28 +0600

    fourth commit

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 
 const Dashboard = () => {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const toggleDrawer = () => {
     setIsDrawerOpen(!isDrawerOpen);
@@ -12,7 +12,7 @@ const Dashboard = () => {
   return (
     <div>
       <div className="text-center">
-      <button
+        <button
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           type="button"
           onClick={toggleDrawer}
