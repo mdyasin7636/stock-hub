2723d59f8f622007fcca5d2add05938fc475e37a
istiaq-tanim <istiaq.tanim17@gmail.com>
2023-09-14 12:56:01 +0600

    social again

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,12 +2,15 @@ import { signOut } from "firebase/auth";
 import auth from "../../../firebase/firebase.config";
 import { useDispatch } from "react-redux";
 import { logOut } from "../../../features/auth/authSlices";
+import { useNavigate } from "react-router";
 
 const Home = () => {
     const dispatch = useDispatch()
+    const navigate=useNavigate()
     const handleLogout = () => {
         signOut(auth)
         dispatch(logOut())
+        navigate("/login")
     }
     return (
         <div>
