a0e209a7fdfb78f2d538a8c54b87517feaf8bc5d
mdyasin111 <mdyasin7636@gmail.com>
2023-09-14 13:03:46 +0600

    new

--- src/Pages/SocialLogin/SocialLogin.jsx
+++ src/Pages/SocialLogin/SocialLogin.jsx
@@ -4,7 +4,7 @@ const SocialLogin = () => {
     return (
         <div>
      
-      <button className="border py-3 w-full bg-gradient-to-r from-gray-700 via-gray-900 to-black rounded-md text-white text-center font-semibold"> <span>
+      <button className="btn w-full bg-gradient-to-r from-gray-700 via-gray-900 to-black rounded-md text-white text-center font-semibold"> <span>
             
             <FcGoogle />
           </span> Register With Google</button>
