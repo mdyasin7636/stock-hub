d119036d5d3bc53a0619d74c946c715bd68ec888
mdyasin111 <mdyasin7636@gmail.com>
2023-09-21 16:22:30 +0600

    login route error fixed and error page design added

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,27 @@
+import Lottie from "lottie-react";
+import { Link } from "react-router-dom";
+import errorPage from "../../assets/error.json";
 
 const ErrorPage = () => {
-    return (
-        <div className="text-center text-3xl text-red-500">
-             <h1>404</h1>
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="container text-center">
+        <Lottie
+          className="mx-auto w-4/12"
+          animationData={errorPage}
+          loop={true}
+        />
+        <div className="mt-10">
+          <Link
+            to="/"
+            className="text-white bg-gradient-to-r from-gray-700 via-gray-900 to-black rounded-md p-4 font-semibold "
+          >
+            Back to Homepage
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
