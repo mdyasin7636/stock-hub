9f13a9171b553040a151936e09481d57bde2035d
mdyasin111 <mdyasin7636@gmail.com>
2023-09-25 10:39:52 +0600

    create purchase added

--- src/Pages/Dashboard/Purchases/CreatePurchase/CreatePurchase.jsx
+++ src/Pages/Dashboard/Purchases/CreatePurchase/CreatePurchase.jsx
@@ -1,10 +1,34 @@
-
 const CreatePurchase = () => {
-    return (
-        <div>
-            <h1>Create Purchase here</h1>
+  return (
+    <div className="grid grid-cols-7 gap-4">
+      <div className="col-span-5">
+        <div className="bg-gray-200 p-4 h-screen">
+          <div className="flex justify-between text-center">
+            <div className="w-full">
+              <input
+                type="text"
+                placeholder="Search by Product Name"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </div>
+            <div className="w-full">
+              <select className="select select-bordered w-full max-w-xs">
+                <option disabled selected>
+                  All Categories
+                </option>
+                <option>Han Solo</option>
+                <option>Greedo</option>
+              </select>
+            </div>
+          </div>
+          Show Products Here
         </div>
-    );
+      </div>
+      <div className="col-span-2">
+        <div className="bg-blue-200 p-4 h-screen">Place Order</div>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePurchase;
