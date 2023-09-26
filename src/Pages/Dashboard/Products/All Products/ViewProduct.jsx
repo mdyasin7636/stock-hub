1be445827868ecc9ea2779b52fcf05a36ff29236
mdyasin111 <mdyasin7636@gmail.com>
2023-09-26 21:20:06 +0600

    updated

--- src/Pages/Dashboard/Products/All Products/ViewProduct.jsx
+++ src/Pages/Dashboard/Products/All Products/ViewProduct.jsx
@@ -1,36 +1,94 @@
+
 const ViewProduct = () => {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <div>
-                <div className='grid md:grid-cols-2 gap-8 '>
-                    <div className="overflow-x-auto">
-                        <table className="table text-center w-full">
-                            {/* Add the w-full and md:w-auto classes to control the width */}
-                            <thead className="bg-gray-300 text-sm font-normal text-gray-700">
-                                <tr>
-                                    <th>Type</th>
-                                    <th>Single</th>
-                                </tr>
-                            </thead>
-                            <tbody className="bg-white text-center dark:border-gray-700 dark:bg-gray-800">
-                                <tr>
-                                    <td>Brand</td>
-                                    <td>Samsung</td>
-                                </tr>
-                                <tr>
-                                    <td>Brand</td>
-                                    <td>Samsung</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div>
-                        <img className="w-60" src="https://i.ibb.co/0fdGLNw/img.png" alt="" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+        <h1 className="text-center text-2xl font-semibold mt-10">Product Details</h1>
+        <div className="flex justify-center items-center gap-20 mt-16">
+        
+        {/* Table */}
+        <div>
+          <div className="relative overflow-x-auto shadow-2xl sm:rounded-lg">
+            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+              <thead className="text-xs text-gray-700 uppercase dark:text-gray-400"></thead>
+              <tbody>
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-24 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                  >
+                    Product Name
+                  </th>
+                  <td className="px-24 py-4">Silver</td>
+                </tr>
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-24 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                  >
+                    Product Code
+                  </th>
+                  <td className="px-24 py-4">White</td>
+                </tr>
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-24 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                  >
+                    Brand
+                  </th>
+                  <td className="px-24 py-4">Black</td>
+                </tr>
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-24 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                  >
+                    Category
+                  </th>
+                  <td className="px-24 py-4">Gray</td>
+                </tr>
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-24 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                  >
+                    Price
+                  </th>
+                  <td className="px-24 py-4">Red</td>
+                </tr>
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-24 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                  >
+                    Quantity
+                  </th>
+                  <td className="px-24 py-4">Red</td>
+                </tr>
+                <tr>
+                  <th
+                    scope="row"
+                    className="px-24 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                  >
+                    Description
+                  </th>
+                  <td className="px-24 py-4">Red</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-    );
+        {/* Image */}
+        <div>
+          <img
+            className="w-72"
+            src="https://i.ibb.co/QbKT5Zn/redmi-note-13-pro.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ViewProduct;
