7fe9a1d2f784c0f5af5d00acf93308bdd34993d2
fayezf <mohammadfayezahmed835@gmail.com>
2023-09-29 23:30:22 +0600

    shipping and discount design added

--- src/Pages/Dashboard/Purchases/CreatePurchase/CreatePurchase.jsx
+++ src/Pages/Dashboard/Purchases/CreatePurchase/CreatePurchase.jsx
@@ -67,20 +67,48 @@ const CreatePurchase = () => {
       </div>
       <div className="col-span-2 bg-gray-100">
         {cartItems.length > 0 ? cartItems?.map(item => <CartItems key={item._id} product={item}></CartItems>) : <h3>There is no product</h3>}
-        <div className="flex justify-center items-end gap-4 mt-2">
+        <div className="md:flex justify-center items-end gap-4 mt-2">
           <p className="text-slate-400 font-medium text-sm">Sub Total</p>
           <p className="text-slate-400 font-medium text-sm">${quantityAmount}</p>
         </div>
-        <div className="flex justify-evenly items-end gap-4 mt-2">
+        <div className="md:flex justify-evenly items-end gap-4 mt-2">
           <p className="text-slate-400 font-medium text-sm">Tax</p>
           <p className="text-slate-400 font-medium text-sm">$0.00</p>
         </div>
+        <div className="md:flex justify-evenly items-end gap-4 mt-2">
+          <p className="text-slate-400 font-medium text-sm">Shipping</p>
+          <p className="text-slate-400 font-medium text-sm">$0.00</p>
+        </div>
+        <div className="md:flex justify-evenly items-end gap-4 mt-2">
+          <p className="text-slate-400 font-medium text-sm">Discount</p>
+          <p className="text-slate-400 font-medium text-sm">$0.00</p>
+        </div>
         <hr className="mt-2" />
-        <div className="flex justify-evenly items-end gap-4 mt-2">
+        <div className="md:flex justify-evenly items-end gap-4 mt-2">
           <p className="text-lg font-bold">Total</p>
           <p className="text-lg font-bold">${quantityAmount}</p>
         </div>
-        <div className="text-end mt-4">
+        <div className="md:flex justify-center gap-2 px-2 mt-4">
+          <div className="md:flex gap-2">
+            <div className="dropdown dropdown-top">
+              <label tabIndex={0} className="btn btn-sm hover:text-white bg-white hover:bg-gradient-to-r from-gray-700 via-gray-900 to-black font-semibold">Shipping</label>
+              <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded w-48">
+                <div className="md:flex justify-center text-center items-center me-2 mt-1">
+                <input type="text" style={{ width: '130px' }} placeholder="Amount" className="bg-white p-2 mx-2" />
+                <input className='btn-xs text-slate-400 bg-white text-sm font-semibold' type="submit" value="Flat" />
+                </div>
+              </ul>
+            </div>
+            <div className="dropdown dropdown-top">
+              <label tabIndex={0} className="btn btn-sm hover:text-white bg-white hover:bg-gradient-to-r from-gray-700 via-gray-900 to-black font-semibold">Discount</label>
+              <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded w-48">
+                <div className="md:flex justify-center text-center items-center me-2 mt-1">
+                <input type="text" style={{ width: '130px' }} placeholder="Amount" className="bg-white p-2 mx-2" />
+                <input className='btn-xs text-slate-400 bg-white text-sm font-semibold' type="submit" value="Flat" />
+                </div>
+              </ul>
+            </div>
+          </div>
           <button className="btn btn-sm text-white bg-gradient-to-r from-gray-700 via-gray-900 to-black font-semibold">
             Place Order
           </button>
